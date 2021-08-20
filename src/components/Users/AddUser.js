49ff36d213fffd676717340d1637ee7f315cc2d5
sofia durkan <sofiadurkan@gmail.com>
2021-08-20 12:32:59 -0500

    button added

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
+import Button from '../UI/Button';
 
 
 const AddUser = props => {
+    const [enteredUsername,setEnteredUsername] = useState('');
+    const [enteredAge,setEnteredAge] = useState('');
+
+
     const AddUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredUsername,enteredAge)
 
     }
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value)
+
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value)
+
+    }
+
+
+
     return(
         <Card className ={classes.input}>
         <form onSubmit ={AddUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
+            <input id="username" type="text" onChange={usernameChangeHandler}/>
             <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number" />
-            <button type="submit">Add User</button>
+            <input id="age" type="number" onChange={ageChangeHandler} />
+            <Button type="submit">Add User</Button>
         </form>
         </Card>
     )
